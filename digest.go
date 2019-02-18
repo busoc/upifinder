466ba649b51a1d32608da8bb34eaa315bd474363
nico <brun.nicolas21@gmail.com>
2019-02-18 14:24:20 +0100

    compute digest from file and/or tar

--- digest.go
+++ digest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/tar"
 	"bytes"
 	"crypto/md5"
 	"encoding/binary"
@@ -11,7 +12,6 @@ import (
 	"time"
 
 	"github.com/midbel/cli"
-	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -32,6 +32,14 @@ var (
 	TIFF = []byte("TIFF")
 )
 
+type Digest struct {
+	File     string
+	Magic    [4]byte
+	Sum      []byte
+	Time     uint64
+	Sequence uint32
+}
+
 var digestCommand = &cli.Command{
 	Usage: "digest <datadir>",
 	Alias: []string{"sum", "cksum"},
@@ -43,67 +51,93 @@ func runDigest(cmd *cli.Command, args []string) error {
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
 	}
-	sema := make(chan struct{}, 8)
-	var grp errgroup.Group
-	for p := range retrPaths(cmd.Flag.Arg(0)) {
-		sema <- struct{}{}
-		p := p
-		grp.Go(func() error {
-			defer func() {
-				<-sema
-			}()
-			r, err := os.Open(p)
+	for d := range retrPaths(cmd.Flag.Arg(0)) {
+		n := GPS.Add(time.Duration(d.Time)).Format("2006-01-02 15:04:05.000")
+		log.Printf("%s | %7d | %s | %x | %s", bytes.Trim(d.Magic[:], "\x00"), d.Sequence, n, d.Sum, d.File)
+	}
+	return nil
+}
+
+func digestReader(r io.Reader) (*Digest, error) {
+	var d Digest
+	if _, err := r.Read(d.Magic[:]); err != nil {
+		return nil, err
+	}
+	var buffer bytes.Buffer
+	if _, err := io.CopyN(&buffer, r, skipBytes(d.Magic[:])); err != nil {
+		return nil, err
+	}
+	digest := md5.New()
+	if _, err := io.Copy(digest, r); err != nil {
+		return nil, err
+	}
+	d.Sum = digest.Sum(nil)
+
+	binary.Read(&buffer, binary.BigEndian, &d.Sequence)
+	binary.Read(&buffer, binary.BigEndian, &d.Time)
+
+	return &d, nil
+}
+
+func retrPaths(base string) <-chan *Digest {
+	q := make(chan *Digest)
+	go func() {
+		defer close(q)
+		filepath.Walk(base, func(p string, i os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			defer r.Close()
+			if i.IsDir() {
+				return nil
+			}
+			if filepath.Ext(p) == ".xml" {
+				return nil
+			}
+			switch e := filepath.Ext(p); e {
+			case ".xml":
+			case ".tar":
+				r, err := os.Open(p)
+				if err != nil {
+					return err
+				}
+				defer r.Close()
 
-			magic := make([]byte, 4)
-			if _, err := r.Read(magic); err != nil {
-				return err
+				tr := tar.NewReader(r)
+				for {
+					h, err := tr.Next()
+					if err == io.EOF {
+						break
+					}
+					if err != nil {
+						return err
+					}
+					if filepath.Ext(h.Name) == ".xml" {
+						continue
+					}
+					d, err := digestReader(io.LimitReader(tr, h.Size))
+					if err != nil {
+						return err
+					}
+					d.File = filepath.Base(h.Name)
+					q <- d
+				}
+			default:
+				r, err := os.Open(p)
+				if err != nil {
+					return err
+				}
+				defer r.Close()
+				d, err := digestReader(r)
+				if err != nil {
+					return err
+				}
+				d.File = filepath.Base(p)
+				q <- d
 			}
-			var buffer bytes.Buffer
-			if _, err := io.CopyN(&buffer, r, skipBytes(magic)); err != nil {
-				return err
-			}
-			digest := md5.New()
-			if _, err := io.Copy(digest, r); err != nil {
-				return err
-			}
-			var (
-				unix     uint64
-				sequence uint32
-			)
-			binary.Read(&buffer, binary.BigEndian, &sequence)
-			binary.Read(&buffer, binary.BigEndian, &unix)
-
-			n := GPS.Add(time.Duration(unix)).Format("2006-01-02 15:04:05.000")
-			log.Printf("%s | %7d | %s | %x | %s", bytes.Trim(magic, "\x00"), sequence, n, digest.Sum(nil), filepath.Base(p))
 			return nil
 		})
-	}
-	return grp.Wait()
-}
-
-func retrPaths(base string) <-chan string {
-  q := make(chan string)
-  go func() {
-    defer close(q)
-    filepath.Walk(base, func(p string, i os.FileInfo, err error) error {
-      if err != nil {
-        return err
-      }
-      if i.IsDir() {
-        return nil
-      }
-      if filepath.Ext(p) == ".xml" {
-        return nil
-      }
-      q <- p
-      return nil
-    })
-  }()
-  return q
+	}()
+	return q
 }
 
 func skipBytes(magic []byte) int64 {
